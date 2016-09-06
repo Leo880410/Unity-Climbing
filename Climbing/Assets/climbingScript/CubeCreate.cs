1d5757a6e2e6045d165999a268549f444a5be177
Leo880410 <leo8804102004@yahoo.com.tw>
2016-09-06 17:27:15 +0800

    first commit
    Added Unity Project to GitHub

---
+++ Climbing/Assets/climbingScript/CubeCreate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCreate : MonoBehaviour {
+
+    public GameObject _ObjCreat;
+    private Vector3 _CreatPosition;
+    private GameObject _ObjClone;
+    public static CubeCreate _instance = null;
+	// Use this for initialization
+	void Start () {
+        _CreatPosition = new Vector3();
+        _CreatPosition.z = 10f;
+        CreatNewCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void CreatNewCube()
+    {
+        _CreatPosition.x = Random.Range(-5f, 5f);
+        _CreatPosition.y = Random.Range(-5f, 5f);
+        _CreatPosition.z = 10f;
+        Instantiate(_ObjCreat, _CreatPosition, Quaternion.identity);
+    }
+}
---
+++ Climbing/Assets/climbingScript/CubeDestory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeDestory : MonoBehaviour {
+
+    public GameObject CreatManagerObject;
+    private CubeCreate CreatManager;
+
+	// Use this for initialization
+	void Start () {
+        CreatManager = CreatManagerObject.GetComponent<CubeCreate>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "HandLeft" || collision.gameObject.tag =="HandRight")
+        {
+            DestoryAndCreat();
+        }
+    }
+    void DestoryAndCreat()
+    {
+        CreatManager.CreatNewCube();
+        Destroy(gameObject);
+    }
+}
